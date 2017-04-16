e032acd14d44e144adbdda0e25a1ef3a74afbca4
Joey <b3tanyan@gmail.com>
2017-04-16 11:14:46 -0400

    Add permission check for punish command

--- SpigotSide/src/main/java/xyz/betanyan/carribeanbans/PunishCommand.java
+++ SpigotSide/src/main/java/xyz/betanyan/carribeanbans/PunishCommand.java
@@ -16,29 +16,35 @@ public class PunishCommand implements CommandExecutor {
         if (sender instanceof Player) {
 
             Player player = (Player) sender;
+            
+            if (player.hasPermission("punish.use")) {
 
-            if (args.length == 0) {
+                if (args.length == 0) {
 
-                player.sendMessage(ChatColor.RED + "Correct syntax: /punish <name>");
-
-            } else {
-
-                String toPunish = args[0];
-
-                if (toPunish.length() <= 16) {
-
-                    InventoryBuilder builder = new InventoryBuilder("&ePunish: &a" + toPunish, 27);
-                    builder.setItem(10, new ItemBuilder(Material.DIAMOND_SWORD).name("&aBan").build());
-                    builder.setItem(12, new ItemBuilder(Material.IRON_SWORD).name("&aMute").build());
-                    builder.setItem(14, new ItemBuilder(Material.GOLDEN_APPLE).name("&aWarn").build());
-                    builder.setItem(16, new ItemBuilder(Material.IRON_BOOTS).name("&aKick").build());
-
-                    player.openInventory(builder.build());
+                    player.sendMessage(ChatColor.RED + "Correct syntax: /punish <name>");
 
                 } else {
-                    sender.sendMessage(ChatColor.RED + "The username " + args[0] + " is too long!");
-                }
 
+                    String toPunish = args[0];
+
+                    if (toPunish.length() <= 16) {
+
+                        InventoryBuilder builder = new InventoryBuilder("&ePunish: &a" + toPunish, 27);
+                        builder.setItem(10, new ItemBuilder(Material.DIAMOND_SWORD).name("&aBan").build());
+                        builder.setItem(12, new ItemBuilder(Material.IRON_SWORD).name("&aMute").build());
+                        builder.setItem(14, new ItemBuilder(Material.GOLDEN_APPLE).name("&aWarn").build());
+                        builder.setItem(16, new ItemBuilder(Material.IRON_BOOTS).name("&aKick").build());
+
+                        player.openInventory(builder.build());
+
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "The username " + args[0] + " is too long!");
+                    }
+
+                }
+                
+            } else {
+                sender.sendMessage(ChatColor.RED + "No permission.");
             }
         } else {
             sender.sendMessage(ChatColor.RED + "Only players can punish.");
